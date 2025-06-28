d3720de59542a2ac9d56cc46ea16ef5de9f54b37
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-06-29 02:33:01 +0530

    Naviagtion file

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-between">
+      <div className="font-bold text-lg">Proof-Port</div>
+      <div className="space-x-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
