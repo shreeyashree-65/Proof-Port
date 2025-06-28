ca8f46a9526e8860743c53d228aa8eb72476a066
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-06-29 03:10:26 +0530

    Addded router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import About from './pages/About';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          {/* Navigation */}
+          <nav className="bg-blue-600 p-4 text-white flex gap-4">
+            <Link className="hover:underline" to="/">Home</Link>
+            <Link className="hover:underline" to="/about">About</Link>
+            <Link className="hover:underline" to="/dashboard">Dashboard</Link>
+          </nav>
+
+          {/* Routes */}
+          <div className="p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
 
