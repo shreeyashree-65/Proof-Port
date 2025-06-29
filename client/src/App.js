68b318bd1013aab1e410c26f194d3250700b55fe
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-06-29 11:01:20 +0530

    Modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          {/* Navigation */}
-          <nav className="bg-blue-600 p-4 text-white flex gap-4">
-            <Link className="hover:underline" to="/">Home</Link>
-            <Link className="hover:underline" to="/about">About</Link>
-            <Link className="hover:underline" to="/dashboard">Dashboard</Link>
-          </nav>
-
-          {/* Routes */}
-          <div className="p-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
