8df5ba47109efe4cdefec0c5f977dcbedd0a743e
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-07-01 23:53:14 +0530

    Changes

--- client/src/utils/contract.js
+++ client/src/utils/contract.js
@@ -1,18 +1,15 @@
 import { ethers } from "ethers";
-import ProofPortAbi from "./ProofPortAbi.json"; // we'll create this next
+import ProofPortAbi from "./ProofPortAbi.json";
 
-// Replace with your deployed contract address
-const CONTRACT_ADDRESS = "YOUR_DEPLOYED_CONTRACT_ADDRESS_HERE";
-
-// Connect to provider (Metamask injected provider)
-export function getContract() {
+export async function getContract() {
   if (!window.ethereum) {
     alert("Please install MetaMask to use this app.");
     throw new Error("MetaMask not installed");
   }
 
+  const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
   const provider = new ethers.BrowserProvider(window.ethereum);
-  const signer = provider.getSigner();
+  const signer = await provider.getSigner();
   const contract = new ethers.Contract(CONTRACT_ADDRESS, ProofPortAbi, signer);
 
   return { contract, provider, signer };
